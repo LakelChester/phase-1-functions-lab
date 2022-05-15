80aac8c4b52d732a19ad62a45d8bb6300c3c5676
LakelChester <lakel@chesterassociate.com>
2022-05-14 18:52:19 -0700

    commit tree eb755c7b007f7f1eae2740fcf5b786588f47e57e

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(pickup) {
+    let distanceFromHq = pickup - 42
+        if(distanceFromHq < 0){
+            return distanceFromHq * -1
+        }
+    return distanceFromHq
+}
+
+function distanceFromHqInFeet(blocks){
+   return distanceFromHqInBlocks(blocks) * 264
+}
+
+function distanceTravelledInFeet(start, destination){
+    let distanceTravelled = (destination - start) * 264
+        if (distanceTravelled < 0){
+            return distanceTravelled * -1
+        }
+    return distanceTravelled
+}
+
+function calculatesFarePrice(start, destination){
+    
+    let distanceTravelledPricing = distanceTravelledInFeet(start,destination)
+    
+    if (distanceTravelledPricing < 400){
+        return 'Free sample!'
+    
+    } else if(distanceTravelledPricing > 400 && distanceTravelledPricing < 2000){
+        return (distanceTravelledPricing - 400) * .02
+    
+    } else if(distanceTravelledPricing > 2000 && distancetravelledPricing < 2500){
+        return 'Flat Fee'
+    }
+        return 'Cannot travel that far'
+    
+}
+
